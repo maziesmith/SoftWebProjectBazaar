42b73c71767ec7eb2ce444a5c1e213112c0bc27a
Karolis <Karolis@DESKTOP-CN8AGQM>
2017-02-07 12:09:17 +0100

    Add project files.

---
+++ SoftWebProjectBazaar/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoftWebProjectBazaar.Startup))]
+namespace SoftWebProjectBazaar
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
