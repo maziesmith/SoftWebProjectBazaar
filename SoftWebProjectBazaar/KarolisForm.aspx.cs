71dbc333228c055d7436b51bbe47046c1a8b0715
Bitharis <bitharis@gmail.com>
2017-02-07 12:54:56 +0100

    conflict from Panos

--- SoftWebProjectBazaar/KarolisForm.aspx.cs
+++ SoftWebProjectBazaar/KarolisForm.aspx.cs
@@ -10,5 +10,10 @@ namespace SoftWebProjectBazaar {
         protected void Page_Load(object sender, EventArgs e) {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
